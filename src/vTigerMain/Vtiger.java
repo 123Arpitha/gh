527d573d9a2ef51abb829f2cdaa11496fd0abd35
123Arpitha <arpithaggowda3108@gmail.com>
2022-01-27 15:28:14 +0530

    modified vtiger

--- src/vTigerMain/Vtiger.java
+++ src/vTigerMain/Vtiger.java
@@ -2,15 +2,16 @@ package vTigerMain;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import reasdingData.ExcelLib;
 import vTiger.LoginPage;
 
 public class Vtiger {
-	
-	public static void delete_MultipleInvoiceDetails_FromListView() {
+	@Test
+	public  void delete_MultipleInvoiceDetails_FromListView() {
 		String testURL = ExcelLib.readStringData("Sheet3", 0, 0);
 		String username = ExcelLib.readStringData("Sheet3", 0, 1);
 		String password = ExcelLib.readStringData("Sheet3", 0, 2);
